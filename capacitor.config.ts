51402168af661ced67ccb88bb014fb9aeba8053c
tarunfc7 <tarun@fixigo.in>
2023-01-14 15:06:02 +0400

    First Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Fixigotest',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
