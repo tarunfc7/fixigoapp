0b755a1c1d93fc7e49d81a1af2c438a8a9230601
tarunfc7 <tarun@fixigo.in>
2023-01-14 15:06:26 +0400

    code commit v1.0

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,27 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.fixigo.apptest',
   appName: 'Fixigotest',
   webDir: 'www',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      // backgroundColor: "#ffffffff",
+      androidSplashResourceName: "splash",
+      // androidScaleType: "CENTER_CROP",
+      showSpinner: true,
+      // androidSpinnerStyle: "large",
+      // iosSpinnerStyle: "small",
+      // spinnerColor: "#999999",
+      splashFullScreen: true,
+      splashImmersive: true,
+      // layoutName: "launch_screen",
+      // useDialog: true,
+    },
+  },
 };
 
 export default config;
